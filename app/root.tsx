e6c5c4218292955ee742e33e330385d2f1d65051
srikar-samudrala <srikar@eve.world>
2022-06-06 17:31:31 +0530

    sets up tailwind

--- app/root.tsx
+++ app/root.tsx
@@ -8,11 +8,17 @@ import {
   ScrollRestoration,
 } from '@remix-run/react';
 
+import styles from '~/styles/app.css';
+
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
   title: 'shortie - url shortener',
   viewport: 'width=device-width,initial-scale=1',
 });
+
+export function links() {
+  return [{ rel: 'stylesheet', href: styles }];
+}
 
 export default function App() {
   return (
