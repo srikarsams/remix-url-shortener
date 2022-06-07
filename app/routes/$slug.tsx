3df20bd457a643b028a4db5f14f72154c632086f
srikar-samudrala <srikar@eve.world>
2022-06-07 17:34:01 +0530

    sets up redirect route

---
+++ app/routes/$slug.tsx
@@ -0,0 +1,23 @@
+import type { LoaderFunction } from '@remix-run/node';
+import { redirect } from '@remix-run/node';
+import { db } from '~/utils/db.server';
+
+export const loader: LoaderFunction = async ({ params }) => {
+  const slug = params.slug;
+  console.log({ slug });
+  const url = await db.url.findFirst({
+    where: {
+      slug,
+    },
+  });
+
+  if (!url) {
+    return redirect('/');
+  }
+
+  return redirect(url.url, {
+    headers: {
+      'Cache-Control': 'public, max-age=604800',
+    },
+  });
+};
